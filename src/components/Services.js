0cd739201879ba86fde402fa962d4258f50bb7a9
Jass <frontendjass@gmail.com>
2024-02-07 13:21:51 -0600

    Implemented enhancements to the services section, including the addition of a wave spacer for improved visual appeal.

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,24 +1,87 @@
-// Services.js
-import React from 'react';
-import Service from './Service';
+import React from "react";
 
 function Services() {
   const servicesData = [
-    { title: 'Full Grooming Package', details: ['Bathing', 'Haircut', 'Nail trimming', 'Ear cleaning', 'Teeth brushing', 'etc.'] },
-    { title: 'Express Grooming', details: ['Quick touch-ups for a clean look', 'Ideal for busy pet owners'] },
-    { title: 'Spa Treatments', details: ['Relaxing massages', 'Aromatherapy baths', 'Pawdicures for extra flair'] },
-    // Add more services as needed
+    {
+      title: "Full Grooming Package",
+      details: [
+        "Bathing",
+        "Haircut",
+        "Nail trimming",
+        "Ear cleaning",
+        "Teeth brushing",
+        "etc.",
+      ],
+    },
+    {
+      title: "Express Grooming",
+      details: [
+        "Quick touch-ups for a clean look",
+        "Ideal for busy pet owners",
+      ],
+    },
+    {
+      title: "Spa Treatments",
+      details: [
+        "Relaxing massages",
+        "Aromatherapy baths",
+        "Pawdicures for extra flair",
+      ],
+    },
+
   ];
 
   return (
+    <div>
     <section id="services">
-      <h2 className='services-title'>Our Services</h2>
+      <h2 className="services-title">Our Services</h2>
       <div className="services-container">
-        {servicesData.map((service, index) => (
-          <Service key={index} title={service.title} details={service.details} />
-        ))}
+
+        <div className="service">
+
+          <div className="service-details">
+            <ul>
+              <li>Bathing</li>
+              <li>Haircut</li>
+              <li>Nail trimming</li>
+              <li>Ear cleaning</li>
+              <li>Teeth brushing</li>
+            </ul>
+          </div>
+
+          <h3 className="service-title">Full Grooming Package</h3>
+
+        </div>
+        <div className="service">
+
+          <div className="service-details">
+            <ul>
+              <li>Quick touch-ups for a clean look</li>
+              <li>Ideal for busy pet owners</li>
+            </ul>
+          </div>
+
+          <h3 className="service-title">Express Grooming</h3>
+          
+        </div>
+        <div className="service">
+
+          <div className="service-details">
+            <ul>
+              <li>Relaxing massages</li>
+              <li>Aromatherapy baths</li>
+              <li>Pawdicures for extra flair</li>
+            </ul>
+          </div>
+
+          <h3 className="service-title">Full Grooming Package</h3>
+          
+        </div>
       </div>
+      
     </section>
+    <div className="spacer layer2"></div>
+    </div>
   );
 }
 
